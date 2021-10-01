d6abfe92fd044cb72716b8fa2aca913c0e31ce1b
Winterman981 <teerathanomnuea@gmail.com>
2021-10-02 00:39:30 +0700

    Added volume slider

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -31,7 +31,8 @@ public class AudioManager : MonoBehaviour
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
-		}
+            s.source.outputAudioMixerGroup = s.group;
+        }
     }
 
     public void Start()
